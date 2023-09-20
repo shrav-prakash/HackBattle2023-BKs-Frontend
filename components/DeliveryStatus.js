f5a70eb4cffc5976542b0d4142db8c913ca9b377
salt57 <sourishgupta02@gmail.com>
2023-09-21 01:27:26 +0530

    fix: fixed backend calls

--- components/DeliveryStatus.js
+++ components/DeliveryStatus.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
 import { Pressable, Text, TextField } from "react-native";
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function DeliveryStatus() {
   const [deliverer, setDeliverer] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
   const checkPickupStatus = async () => {
     setIsLoading(true);
+    const token = await AsyncStorage.getItem("token");
     const response = await axios.get(
       "http:/192.168.168.176:3001/api/user/checkPickupStatus",
       {
--- components/PendingPickups.js
+++ components/PendingPickups.js
@@ -2,14 +2,22 @@ import { useState, useEffect } from "react";
 import BgImg from "./BgImg";
 import { Text, FlatList } from "react-native";
 import Delivery from "./Delivery";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function PendingPickups() {
   const [pickups, setPickups] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const getAcceptedPickups = async () => {
     setIsLoading(true);
+    const token = await AsyncStorage.getItem("token");
     const response = await axios.get(
-      "https:/192.168.168.176:3001/api/user/acceptedPickups"
+      "https:/192.168.168.176:3001/api/user/acceptedPickups",
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
     );
     setPickups(response.data.pickups);
     setIsLoading(false);
